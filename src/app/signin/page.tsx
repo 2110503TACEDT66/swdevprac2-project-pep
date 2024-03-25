fb34491105d538d4305e9b24c73c7791711fb6b0
Witchapat Jindanath <jwitchapat@gmail.com>
2024-03-25 17:35:51 +0700

    signinpage

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import SignInForm from '../../components/SignInForm';
 
@@ -12,19 +11,3 @@ function SignInPage() {
 }
 
 export default SignInPage;
-||||||| 049f6a2
-=======
-import React from 'react';
-import SignInForm from '../../components/SignInForm';
-
-function SignInPage() {
-  return (
-    <div>
-      <SignInForm />
-      <p>If you don't have an account, <a href="/signup">sign up here</a>.</p>
-    </div>
-  );
-}
-
-export default SignInPage;
->>>>>>> 3e6731779a92545f65cfdb347b61eb329f76d730
