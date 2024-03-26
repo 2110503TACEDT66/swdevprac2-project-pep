ec418d70cc3bf72f65fa5b3d3dadef9d4afd8eb6
nathaฯ <nathachumsri@gmail.com>
2024-03-26 17:51:22 +0700

    add delete review

--- src/components/CampgroundDetail.tsx
+++ src/components/CampgroundDetail.tsx
@@ -1,19 +1,64 @@
-import Image from "next/image"
-import Link from "next/link"
+// import Image from "next/image"
+// import Link from "next/link"
+// import LocationOnIcon from '@mui/icons-material/LocationOn';
+// import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+// import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+// import RatingOverall from "./RatingOverall";
+
+// export default async function CampgroundDetail({ campgroundDetail }: { campgroundDetail: any }) {
+
+//     return (
+//         <div className='bg-cyan-900 justify-between items-center p-0 m-0 h-[95vh]'>
+//             <div className="flex flex-row text-white">
+//                     <div className="h-[95vh] w-[55vw] relative">
+//                         <Image src={campgroundDetail.picture} alt="Campground Image" layout="fill" objectFit="cover" />
+//                     </div>
+                
+//                 <div className="flex-1 mx-5 text-left">
+//                     <div className="text-3xl font-bold my-3">{campgroundDetail.name}</div>
+//                     <div className="text-[16px] my-3">{campgroundDetail.description}</div>
+//                     <p className="text-[16px] my-3 flex items-center">
+//                         <LocationOnIcon className="mr-1" />{campgroundDetail.address}, {campgroundDetail.province}
+//                     </p>
+//                     <p className="text-[16px] my-3 flex items-center">
+//                         <LocalPostOfficeIcon className="mr-1" />{campgroundDetail.postalcode}
+//                     </p>
+//                     <p className="text-[16px] my-3 flex items-center">
+//                         <LocalPhoneIcon className="mr-1" />{campgroundDetail.telephoneNumber}
+//                     </p>
+//                     <Link href={`/booking/${campgroundDetail._id}`}>
+//                         <button className="block bg-sky-600 hover:bg-sky-800 text-white px-3 py-2 shadow-sm mt-4">
+//                             Make Booking
+//                         </button>
+//                     </Link>
+                    
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
-import RatingOverall from "./RatingOverall";
 
-export default async function CampgroundDetail({ campgroundDetail }: { campgroundDetail: any }) {
+export default function CampgroundDetail({ campgroundDetail }: { campgroundDetail: any }) {
+    if (!campgroundDetail) {
+        return null;
+    }
 
     return (
         <div className='bg-cyan-900 justify-between items-center p-0 m-0 h-[95vh]'>
             <div className="flex flex-row text-white">
-                    <div className="h-[95vh] w-[55vw] relative">
+                <div className="h-[95vh] w-[55vw] relative">
+                    {campgroundDetail.picture && (
                         <Image src={campgroundDetail.picture} alt="Campground Image" layout="fill" objectFit="cover" />
-                    </div>
-                
+                    )}
+                </div>
+
                 <div className="flex-1 mx-5 text-left">
                     <div className="text-3xl font-bold my-3">{campgroundDetail.name}</div>
                     <div className="text-[16px] my-3">{campgroundDetail.description}</div>
@@ -31,9 +76,10 @@ export default async function CampgroundDetail({ campgroundDetail }: { campgroun
                             Make Booking
                         </button>
                     </Link>
-                    
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
+
